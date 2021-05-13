5c4351f2086b88d32a6c31db82920d53126151ee
DarcJC <me@darc.pro>
2021-05-14 00:52:08 +0800

    add secret support

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # 微信凭据服务器
 
 ## Usage
-设置环境变量`WX_APPID`与`WX_SECRET`并运行即可。
+设置环境变量`WX_APPID`、`WX_SECRET`以及`SERVER_SECRET`并运行即可。
+
+Endpoint: `yourdomain/token?key=<your_secret>`
+
+## Changelog
+
+**v0.1.1** 添加了`SERVER_SECRET`用于请求鉴权；将部分`static`移动到`AppState`中。
--- src/main.rs
+++ src/main.rs
@@ -1,39 +1,28 @@
-
 #![feature(duration_consts_2)]
 
 use std::{
-    env, time
+    env, time,
 };
 use std::sync::RwLock;
 use lazy_static::lazy_static;
-use actix_web::{get, HttpServer, App, HttpRequest, Responder, Error, HttpResponse};
+use actix_web::{get, web, HttpServer, App, HttpRequest, Responder, Error, HttpResponse};
 use std::future::{
     Ready, ready,
 };
 use serde::{
     Serialize, Deserialize,
 };
-use std::ops::Sub;
 use async_std::task;
 
-static mut APP_ID: Option<String> = None;
-static mut APP_SECRET: Option<String> = None;
 static EXPIRE_TIME: time::Duration = time::Duration::new(3600, 0);
 lazy_static! {
     static ref ACCESS_TOKEN: RwLock<String> = RwLock::new(String::new());
     static ref CREATED_TIME: RwLock<time::Instant> = RwLock::new(time::Instant::now() - time::Duration::new(7200, 0));
 }
 
-fn load_env() {
-    unsafe {
-        APP_ID = Option::from(env::var("WX_APPID").unwrap());
-        APP_SECRET = Option::from(env::var("WX_SECRET").unwrap());
-    }
-}
-
 #[derive(Serialize)]
 struct RespondToken {
-    token: String
+    token: String,
 }
 
 impl RespondToken {
@@ -65,47 +54,58 @@ struct WxTokenRespond {
     expires_in: usize,
 }
 
-async fn renew_token() -> Result<(), ()>{
+async fn renew_token<'a>(appid: &'a str, secret: &'a str) -> Result<(), ()> {
     const ENDPOINT: &str = "https://api.weixin.qq.com/cgi-bin/token";
     let client = reqwest::Client::new();
-    unsafe {
-        let appid = APP_ID.clone().unwrap();
-        let secret = APP_SECRET.clone().unwrap();
-        let res = client
-            .get(ENDPOINT)
-            .query(&[
-                ("grant_type", "client_credential"),
-                ("appid", appid.as_str()),
-                ("secret", secret.as_str()),
-            ])
-            .send()
-            .await
-            .unwrap();
-        let data = res.json::<WxTokenRespond>().await.unwrap();
-        *(ACCESS_TOKEN.write().unwrap()) = data.access_token;
-    }
+    let res = client
+        .get(ENDPOINT)
+        .query(&[
+            ("grant_type", "client_credential"),
+            ("appid", appid),
+            ("secret", secret),
+        ])
+        .send()
+        .await
+        .unwrap();
+    let data = res.json::<WxTokenRespond>().await.unwrap();
+    *(ACCESS_TOKEN.write().unwrap()) = data.access_token;
     Ok(())
 }
 
 #[get("/token")]
-async fn fetch_token(req: HttpRequest) -> impl Responder {
+async fn fetch_token(req: HttpRequest, data: web::Data<AppState>) -> impl Responder {
+
+    let query_str = req.query_string();
+    let qs = qstring::QString::from(query_str);
+    let key = qs.get("key").unwrap_or_default();
+    if key.ne(data.secret.as_str()) {
+        return Err(HttpResponse::Forbidden());
+    }
+
     let now_time = time::Instant::now();
     if now_time.checked_duration_since(*(CREATED_TIME.read()).unwrap()).unwrap().ge(&EXPIRE_TIME) {
-        task::block_on(renew_token());
+        task::block_on(renew_token(&data.wx_appid.as_str(), &data.wx_secret.as_str())).unwrap();
         *(CREATED_TIME.write().unwrap()) = time::Instant::now();
     }
 
-    RespondToken::new((*(ACCESS_TOKEN.read().unwrap())).clone())
+    Ok(RespondToken::new((*(ACCESS_TOKEN.read().unwrap())).clone()))
+}
 
+struct AppState {
+    secret: String,
+    wx_appid: String,
+    wx_secret: String,
 }
 
 #[actix_web::main]
-async fn main() -> std::io::Result<()>  {
-    load_env();
-    // task::block_on(renew_token());
-
+async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
+            .data(AppState {
+                secret: env::var("SERVER_SECRET").unwrap(),
+                wx_appid: env::var("WX_APPID").unwrap(),
+                wx_secret: env::var("WX_SECRET").unwrap(),
+            })
             .service(fetch_token)
     })
         .bind("0.0.0.0:8080")?
